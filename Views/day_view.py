44993e737f373b29f45c355fae52af008c550208
dilansci <dylan.branda1@gmail.com>
2024-01-09 23:30:13 +0000

    Some minor changes. Finish day_view label view

--- Views/day_view.py
+++ Views/day_view.py
@@ -13,6 +13,7 @@ class DayView(ttk.Frame):
         # create widgets here
         # buttons
         self.registers = []
+        self.reg_info = []
         self.days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
         self.day_buttons = []
         
@@ -23,35 +24,40 @@ class DayView(ttk.Frame):
             # this section assigns each button a 'name' and a value from 0-6. The button is then created and gridded.
             self.day_buttons.append(ttk.Button(self, text=self.days[i], command= lambda i=i: self.share_day(self.days[i]) )) #  this command will show classes depending on which day was selected.
             self.day_buttons[i].grid(row=0,column=i, sticky="EW", pady=5) # DONT USE PAKCING!!! - matthew :)
-        self.reg_buttons("Monday")
+        self.reg_widgets("Monday")
 
     def share_day(self, day):
-        self.kill_everything()
-        self.reg_buttons(day)
+        self.kill_everything() # this always goes first :))
+        self.reg_info(day)
+        self.reg_widgets(day)
 
 
-    def reg_buttons(self, day):
+    def reg_widgets(self, day):
         ## VerticalScrolledFrame uses 'self.reg_frame.interior' 
         self.reg_frame = VerticalScrolledFrame(self)
         self.reg_frame.grid(sticky="NESW")
 
         self.columnconfigure(0, weight=1)
 
-        classCount = len(self.control.day_service.get_lessons_day(day))
-        print("Class count",classCount)
-        r = 1
-        for count in range (0,classCount):
-            self.registers.append(ttk.Button(self.reg_frame.interior, text=f"Register({count+1})")) # will give command soon   
+        num_of_classes = self.control.day_service.get_lessons_day(day) 
+        print("Class count",num_of_classes)
+
+        r,c = 1,1
+        for count in range (0,len(num_of_classes)):
+            # buttons
+            self.registers.append(ttk.Button(self.reg_frame.interior, text=f"Register {count+1}")) # will give command soon   
             self.registers[count].grid(row=r, column=0, sticky="EW")
+            # class_info
+            self.reg_widgets.append(ttk.Label(self.reg_frame.interior, text=f"{}") # need to finish this. Loop through num_of_classes and add relavant data from there into this label.
+            self.reg_widgets[count].grid(row=r, column=c, sticky="NESW")
             # when sticky="NESW" it expands the button to be the size of the row. Could use this for the labels to display info on each class within the Register.
             r += 1
+            c += 1
         self.registers.clear()
-
-    def reg_info(self, day):
-        pass
 
     def kill_everything(self):
         self.reg_frame.destroy()
+
 '''
 Lesson --> class_ID --> teacher_ID
                     --> 
