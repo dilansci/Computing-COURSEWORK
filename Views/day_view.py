d91c4e84fcd2faaef78913fef138285e8df1250c
dilansci <dylan.branda1@gmail.com>
2024-01-14 12:45:43 +0000

    Deleted 'column_names' function. Need to remove all traces of services within the "view" files and create functions within controllers which by proxy call service functions.

--- Views/day_view.py
+++ Views/day_view.py
@@ -24,18 +24,11 @@ class DayView(ttk.Frame):
             # this section assigns each button a 'name'. The button is then created and gridded.
             self.day_buttons.append(ttk.Button(self, text=self.days[i], command= lambda i=i: self.share_day(self.days[i]) ))
             self.day_buttons[i].grid(row=0,column=i, sticky="EW", pady=5) # DONT USE PAKCING!!! - matthew :)
-        self.column_names()
         self.reg_widgets("Monday")
 
     def share_day(self, day):
         self.kill_everything() # this always goes first :))
         self.reg_widgets(day)
-
-    def column_names(self):
-        print("COLUMN NAMES HAS RUN!!!")
-        self.reg_col = ttk.Label(self, text="REGISTERS!").grid(row=1, column=0) 
-        self.teach_col = ttk.Label(self, text="TEACHERS!").grid(row=1, column=1)
-        self.level_col = ttk.Label(self, text="LABEL!").grid(row=1, column=2)
         
 
     def reg_widgets(self, day):
@@ -49,20 +42,29 @@ class DayView(ttk.Frame):
         r = 2
         for count in range (0,len(reg_info)):
             print(count)
-            class_info = self.control.reg_service.get_class(reg_info[count][1])
-            print("Class info", class_info)
-            teacher_name = self.control.reg_service.get_teacher_name(class_info[0][1])
-            print(teacher_name)
-            # reg_buttons
+            self.class_info = self.control.reg_service.get_class(reg_info[count][1]) ## Recreate service functions inside respective controllers. i.e. self.control.get_class --> (SERVICE) self.service.get_class
+            print("Class info", self.class_info)
+            self.teacher_name = self.control.reg_service.get_teacher_name(self.class_info[0][1])
+            print(self.teacher_name)
+            self.level_num = self.class_info[0][2]
+            print("Current Level num", self.level_num)
+            # Reg Buttons
             self.registers.append(ttk.Button(self.reg_frame.interior, text=f"Register {count+1}")) # will give command soon   
             self.registers[count].grid(row=r, column=0, sticky="EW")
-            # teacher_name
-            self.class_contents.append(ttk.Label(self.reg_frame.interior, text=f"Teacher: {teacher_name[0][0]} {teacher_name[0][1]}")) # call class_contents
+            # Info
+            self.class_contents.append(ttk.Label(self.reg_frame.interior, text=f"Teacher: {self.teacher_name[0][0]} {self.teacher_name[0][1]}")) # call class_contents
             self.class_contents[count].grid(row=r, column=1, columnspan=2, sticky="NESW")
             # when sticky="NESW" it expands the button to be the size of the row. Could use this for the labels to display info on each class within the Register.
             r += 1
         self.registers.clear()
         self.class_contents.clear()
+
+    def tree_views(self):
+        self.column_names = ("Teacher", "Level", "Time") 
+        self.reg_tree = ttk.Treeview(self, columns=self.column_names, show="headings")
+        self.reg_tree.heading("Teacher", text= (self.teacher_name[0][0] + self.teacher_name[0][1]))
+        self.reg_tree.heading("Level", text=self.class_info[0][2])
+        self.reg_tree.heading("Time",text=)
 
     def kill_everything(self):
         self.reg_frame.destroy()
