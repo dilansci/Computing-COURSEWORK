1252c754930741559fe575dbbe12364bd4f58d9e
dilansci <dylan.branda1@gmail.com>
2024-02-16 14:18:37 +0000

    Clean up

--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
 & C:/Users/dylan/AppData/Local/Microsoft/WindowsApps/python3.11.exe main_app.py
-# Use '.\run.ps1' command in powershell to run code
+# Use '.\run.ps1' command in powershell to run code **FOR LAPTOP**
